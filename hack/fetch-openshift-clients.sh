53ef4e8ec824d09b62fcb9ea52065a8b8ed02bfc
Vincent Demeester <vdemeest@redhat.com>
2022-10-05 16:57:34 +0200

    Download versionned ocp instead of harcoded one
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

--- hack/fetch-openshift-clients.sh
+++ hack/fetch-openshift-clients.sh
@@ -2,6 +2,9 @@
 set -euo pipefail
 set -x
 
+OPENSHIFT_VERSION="$1"
+shift
+
 cd /tmp
 
 ARCH=$(uname -m)
@@ -9,13 +12,13 @@ echo "Fetch openshift-clients for ${ARCH}..."
 
 case "${ARCH}" in
     amd64|x86_64)
-        curl -fsSLO https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable-4.8/openshift-client-linux.tar.gz
+        curl -fsSLO https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable-${OPENSHIFT_VERSION}/openshift-client-linux.tar.gz
         ;;
     ppc64le)
-        curl -fsSLO https://mirror.openshift.com/pub/openshift-v4/ppc64le/clients/ocp/stable-4.8/openshift-client-linux.tar.gz
+        curl -fsSLO https://mirror.openshift.com/pub/openshift-v4/ppc64le/clients/ocp/stable-${OPENSHIFT_VERSION}/openshift-client-linux.tar.gz
     ;;
     s390x)
-        curl -fsSLO https://mirror.openshift.com/pub/openshift-v4/s390x/clients/ocp/stable-4.8/openshift-client-linux.tar.gz
+        curl -fsSLO https://mirror.openshift.com/pub/openshift-v4/s390x/clients/ocp/stable-${OPENSHIFT_VERSION}/openshift-client-linux.tar.gz
     ;;
     *)
         echo "unsupported architecture"
